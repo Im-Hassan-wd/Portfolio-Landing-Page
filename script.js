4880c6c9e5f23b00dcb1b962a5f11e0fdea398ed
Im-Hassan-wd <alhassansahih@gmail.com>
2021-08-29 23:08:18 +0100

    Initializing

---
+++ script.js
@@ -0,0 +1,26 @@
+const name = document.querySelector('.fancy-name');
+const strName = name.textContent;
+const splitName = strName.split('');
+name.textContent = '';
+
+for (let i = 0; i < splitName.length; i++) {
+  name.innerHTML += '<span>'+ splitName[i] +'</span>';
+}
+
+let character = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+  const span = name.querySelectorAll('span')[character];
+  span.classList.add('fancy');
+  character++;
+  if (character === splitName.length) {
+    complete();
+    return;
+  }
+}
+
+function complete(){
+  clearInterval(timer);
+  timer = null;
+}
